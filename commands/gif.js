581b3664746348a0912d7007eb26281cbf9d9b3e
rachultimate <59806140+rachultimate@users.noreply.github.com>
2021-12-09 23:59:27 -0300

    Update gif.js

--- commands/gif.js
+++ commands/gif.js
@@ -1,8 +1,10 @@
+var fs = require('fs');
+
 module.exports = {
     name: 'gif',
     description: 'Send a random killugon gif',
     execute(client, message) {
-        const images = fs.readdirSync('./imgs').filter(file => file.endsWith('.gif') || file.endsWith('.GIF'));
+        const images = fs.readdirSync('.\imgs').filter(file => file.endsWith('.gif') || file.endsWith('.GIF'));
         const randomImage = random(images);
         const attachImg = new MessageAttachment(path + randomImage, randomImage);
 
